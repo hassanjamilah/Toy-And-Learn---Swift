b2fe4edd68a40f6c7dc37cb182b846e1913f9d41
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-03-01 15:49:21 +0800

    Attached View Controllers to the views

---
+++ Toy And Learn/Controller/Toys/Tables' cells/ToyListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToyListTableViewCell.swift
+//  Toy And Learn
+//
+//  Created by user on 01/03/2020.
+//  Copyright © 2020 Andalus. All rights reserved.
+//
+
+import UIKit
+
+class ToyListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Toy And Learn/Controller/Toys/ToysListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToysListViewController.swift
+//  Toy And Learn
+//
+//  Created by user on 01/03/2020.
+//  Copyright © 2020 Andalus. All rights reserved.
+//
+
+import UIKit
+
+class ToysListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
